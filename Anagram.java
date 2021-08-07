108976def78071d5c26674db2142ed0e744490bf
Jara <87618885+Jara18@users.noreply.github.com>
2021-08-08 01:17:27 +0530

    Create Anagram.java

---
+++ Anagram.java
@@ -0,0 +1,21 @@
+//algorithm for string anagrams
+
+public static boolean anagram(String a, String b) {
+		boolean isAnagram = false;
+		boolean visited[] = new boolean[b.length()];
+		if(a.length()==b.length()) {
+			for(int i=0; i<a.length(); i++) {
+				char c = a.charAt(i);
+				isAnagram = false;
+				for(int j=0; j<b.length(); j++) {
+					if(b.charAt(j) == c && !visited[j]) {
+						visited[j] = true;
+						isAnagram = true;
+						break;
+					}
+				}
+				if(!isAnagram) break;
+			}
+		}
+		return isAnagram;
+	}
